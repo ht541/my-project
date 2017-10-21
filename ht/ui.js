c2b38349fef3a893ea796b9925c4989d1e10cacf
DESKTOP-APL1BJF\ht541 <405149678@qq.com>
2017-10-21 11:25:29 +0800

    清单应用

--- ht/ui.js
+++ ht/ui.js
@@ -2,6 +2,8 @@
     'use strict'
     var el_article_list = document.querySelector('#article_lists');
     var el_article_form = document.querySelector('#article_form');
+    var btn_del,btn_update;
+
     init();
 
     function init() {
@@ -34,16 +36,18 @@
         list.forEach(function (article) {
             var el_article = document.createElement('div');
             el_article.innerHTML = `
-             标题:${article.title} ID:${article.id}
-             作者:${article.author}
-                  ${article.content}  
+            <textarea id="" cols="110" rows="30">
+             标题:${article.title}&nbsp${article.id}    作者:${article.author}
+
+             内容:${article.content}
+             </textarea>
             <div>
             <button data-id="${article.id}" id="btn-delete-${article.id}">删除</button>
             <button data-id="${article.id}" id="btn-update-${article}">更新</button>
             </div>
             </br>`;
-            var btn_del = el_article.querySelector('#btn-delete-' + article.id);
-            var btn_update = el_article.querySelector('#btn-update-');
+             btn_del = el_article.querySelector('#btn-delete-' + article.id);
+             btn_update = el_article.querySelector('#btn-update-');
             btn_del.addEventListener('click', function () {
                 var id = parseInt(btn_del.dataset.id)
                 b.del(id);
