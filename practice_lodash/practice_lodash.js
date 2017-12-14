35ac9c8f6ee1194b18059f51734140d52f3fca7f
405149678@qq.com <405149678@qq.com>
2017-12-14 21:56:02 +0800

    17-12-14update

--- practice_lodash/practice_lodash.js
+++ practice_lodash/practice_lodash.js
@@ -1,13 +1,12 @@
 ; (function () {
     'use strict'
 
-    /* Array */
     function chunk(array, size = 1) {
-        const arrLen = array.length;
-        let index = 0,
-            result = [];
-        while (index < arrLen) {
-            result.push(array.slice(index, index + 1))
+        var len = array.length;
+        var result = [];
+        var index = 0;
+        while (index < len) {
+            result.push(array.slice(index, index + size))
             index += size;
         }
         return result;
@@ -57,17 +56,9 @@
         return result;
     }
 
-    function reduce(arr, reducer, values) {
-        var start = values === undefined ? 1 : values;
-        for (var key in arr) {
-            start = reducer(start, arr[key], key, arr);
-        }
-        return start;
-    }
-
     function join(arr, arrjner) {
         return reduce(arr, function (result, value, index) {
-            if (index === arr.length - 1) {
+            if (index == arr.length - 1) {
                 return result + value;
             }
             return result + value + arrjner;
@@ -101,9 +92,10 @@
         }
     }
 
-    function forEach(arr, action) {
-        for (var i = 0; i < arr.length; i++) {
-            action(arr[i], i);
+    function Each(collection, iter) {
+        for (var key in collection) {
+            if (false === iter(collection[key], key, collection))
+                break;
         }
     }
 
@@ -132,8 +124,12 @@
         }
     }
 
-    function teke(arr, n = 1) {
+    function take(arr, n = 1) {
         return arr.slice(0, n);
+    }
+
+    function takeRight(arr, n = 1) {
+        return arr.reverse().slice(0, n).reverse();
     }
 
     function size(obj) {
@@ -143,11 +139,6 @@
             return Object.keys(obj).length;
         }
     }
-
-    function isArrays(test) {
-        return Object.prototype.toString.call(test) === "[object Array]";
-    }
-
 
     function add(augend, addend) {
         return augend + addend;
@@ -177,9 +168,9 @@
     }
 
     function clone(obj) {
-         if(isArrays(obj)){
-             return obj.slice();
-         }
+        if (isArrays(obj)) {
+            return obj.slice();
+        }
         var arr = {};
         for (var key in obj) {
             if (obj.hasOwnProperty(key)) {
@@ -188,20 +179,155 @@
         }
         return arr;
     }
- 
-     function constant(n){
-         return function(){
-             return n;
-         }
-     }
 
-     function first(arr){
-         return arr[0];
-     }
+    function gt(value, other) {
+        return value > other ? true : false;
+    }
 
-     function last(arr){
-         return arr[lengt-1];
-     }
-     
-   
+    function gte(value, other) {
+        return value >= other ? true : false;
+    }
+
+    function isArguments(values) {
+        return Object.prototype.toString.call(values) === "[object Arguments]";
+    }
+
+    function isArrays(test) {
+        return Object.prototype.toString.call(test) === "[object Array]";
+    }
+
+
+    function constant(n) {
+        return function () {
+            return n;
+        }
+    }
+
+    function first(arr) {
+        return arr[0];
+    }
+
+    function last(arr) {
+        return arr[lengt - 1];
+    }
+
+    function reverse(arr) {
+        var r = []
+        for (var i = arr.length - 1; i >= 0; i--) {
+            r.push(arr[i]);
+        }
+        return r
+    }
+
+    function reverseString(str) {
+        var r = '';
+        for (var i = str.length - 1; i >= 0; i--) {
+            r += str[i];
+        }
+        return r;
+    }
+
+
+    function getTypr(values) {
+        return Object.prototype.toString.call(values).slice(8, -1).toLowerCase();
+    }
+
+    function findIndex(arr, predicate, index = 0) {
+        for (var i = 0; i < arr.length; i++) {
+            var value = arr[i];
+            if (predicate(value, i), index)
+                return i
+        }
+        return -1
+    }
+    function find(collection, predicate, index = 0) {
+        for (let key in collection) {
+            if (predicate(collection[key], index)) {
+                return collection[key];
+            }
+        }
+    }
+
+    function indexOf(arr, value) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === value) {
+                return i;
+            }
+        }
+        return -1
+    }
+
+    function reMove(arr, value) {
+        var index = arr.indexOf(value);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+    }
+    function flattenDeep(arr) {
+        while (arr.some(item => Array.isArray(item))) {
+            arr = Array.prototype.concat(...arr);
+        }
+        return arr;
+    }
+
+    function bind(fn, ...fixedArgs) {
+        return function (...receivedArgs) {
+            for (var i = 0; i < fixedArgs.length; i++) {
+                if (fixedArgs[i] === _) {
+                    fixedArgs[i] = receivedArgs.shift();
+                }
+            }
+            var Args = fixedArgs.concat(receivedArgs);
+            return fn(...Args);
+        }
+    }
+
+
+    function reduce(arr, reducer, values) {
+        var start = values === undefined ? 1 : values;
+        for (var key in arr) {
+            start = reducer(start, arr[key], key, arr);
+        }
+        return start;
+    }
+
+
+    function initial(arr) {
+        return arr.splice(0, arr.length - 1, )
+    }
+
+    function containsAll(str, ...srgs) {
+        for (var i = 0; i < srgs.length; i++) {
+            if (str.indexOf(srgs[i]) === -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    function mostTimes(fn, n) {
+        let count = 0;
+        return function (...args) {
+            if (conunt < n) {
+                count++;
+                return fn(...args);
+            }
+        }
+    }
+
+    function constant(value) {
+        return function () {
+            return value;
+        }
+    }
+
+    function identity(value) {
+        return value;
+    }
+
+    function toArray(value) {
+        return [].concat(value);
+    }
+
 })();
